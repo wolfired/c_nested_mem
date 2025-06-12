bef4e9ed4d5c8f70e70f0afe2505a3f21efa58f4
LinkWu <wjianl@qq.com>
2025-06-12 20:38:02 +0800

    chore: xmake settings

--- xmake.lua
+++ xmake.lua
@@ -9,3 +9,10 @@ target("main")
             target:add("cxflags", "/source-charset:utf-8", {force = true})
         end
     end)
+    on_run(function (target)
+        if is_plat("linux") then
+            os.exec("valgrind --leak-check=full --track-origins=yes --read-var-info=yes --trace-children=yes --show-leak-kinds=all --read-inline-info=yes --errors-for-leak-kinds=all --expensive-definedness-checks=yes --gen-suppressions=all --redzone-size=16 --show-error-list=yes " .. target:targetfile())
+        else
+            os.exec(target:targetfile())
+        end
+    end)
